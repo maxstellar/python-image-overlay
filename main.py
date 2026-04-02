eee048b298db49c74fc64b11b050d5af128aa9e7
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-04-01 20:04:35 -0700

    to clarify, last commit meant stripping of the input strings, not anything else. (added comments!)

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from PIL import Image
 
+# take input
+
 basepath = input("Input base image path: ").strip('"\'')
 overlaypath = input("Input overlay image path: ").strip('"\'')
+
+# create image objects
 
 base = Image.open(basepath).convert("RGBA")
 overlay = Image.open(overlaypath).convert("RGBA")
 
+# resize image
+
 overlay_resized = overlay.resize(base.size, Image.Resampling.LANCZOS)
 
+# overlay!
+
 base.paste(overlay_resized, (0, 0), overlay_resized)
+
+# save output
 
 base.save("output.png")
